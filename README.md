dadcf4f86b9b36625097c485bcaadde90cab0804
Dexin Zhou <zhoudexin00@126.com>
2018-01-28 16:03:10 -0800

    Upload
    First upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# california places quiz alexa skills
+# california-places-quiz-alexa-skills
+Alexa Skills - California Places Quiz
+
+Developed by Dexin Zhou in June 2017
+
+Now published! Link below:
+
+https://www.amazon.com/dzhou-California-places-Quiz/dp/B072SMMG97/ref=sr_1_11?s=digital-skills&ie=UTF8&qid=1517183799&sr=1-11&keywords=california
+
+### Detail
+
+* index.js --- main file
+* questions.js --- list of questions
+* alexa-sdk (not included) --- tools to build Alexa Skill
+
---
+++ src/questions.js
@@ -0,0 +1,164 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_GB" : [
+        {
+            "Which city has the famous Balboa Park and a zoo that you can see some pandas?": [
+                "San Diego",
+                "San Francisco",
+                "Los Angeles",
+                "Santa Monica"
+                
+            ]
+        },
+        {
+            "Where is the world-famous Dolby Theater?": [
+                "Hollywood",
+				"Oakland",
+                "Santa Monica",
+				"San Francisco",
+                "New York"
+            ]
+        },
+        {
+            "Where does the Golden Gate Bridge connects?": [
+                 "San Francisco and Marin county",
+				 "San Francisco and Oakland",
+                "San Diego and Coronado",
+                "Palo Alto and Fermont"
+               
+            ]
+        },
+        {
+            "What is the city of Santa Monica known for?": [
+                "beautiful beaches",
+				"the state capitol of California",
+                "Hollywood movies",
+                "the largest city in U.S."
+                
+		        ]
+        },
+		 {
+            "Where is the capitol of California?": [
+                "Sacramento",
+				"Los Angeles",
+                "San Francisco",
+                "Oakland"
+            ]
+        },
+		    {
+            "Where is Yosemite National Park?": [
+                "It is in California, east of San Francisco",
+				"It is in California, east of Los Angeles",
+                "It is in Wyoming, Idaho, and Montana",
+                "It is in California, north of Redding"
+                				
+            ]
+        },
+			 {
+            "Which is not in California?": [
+			    "Grand Canyon national park",
+                "Joshua Tree national park",
+				"Channel Islands national park",
+                "Mojave desert"
+
+            ]
+        },
+        {
+            "Which highway is best known for its beautiful beach scenary along the route?": [
+                "CA-1",
+                "Interstate 5",
+                "Interstate 15",
+				"CA-2"
+            ]
+        }
+    ],
+    "QUESTIONS_EN_US" : [
+        {
+            "Which city has the famous Balboa Park and a zoo that you can see some pandas?": [
+                "San Diego",
+                "San Francisco",
+                "Los Angeles",
+                "Santa Monica"
+                
+            ]
+        },
+        {
+            "Where is the world-famous Dolby Theater?": [
+                "Hollywood",
+				"Oakland",
+                "Santa Monica",
+				"San Francisco",
+                "New York"
+            ]
+        },
+        {
+            "Where does the Golden Gate Bridge connects?": [
+                 "San Francisco and Marin county",
+				 "San Francisco and Oakland",
+                "San Diego and Coronado",
+                "Palo Alto and Fermont"
+               
+            ]
+        },
+        {
+            "What is the city of Santa Monica known for?": [
+                "beautiful beaches",
+				"the state capitol of California",
+                "Hollywood movies",
+                "the largest city in U.S."
+                
+		        ]
+        },
+		 {
+            "Where is the capitol of California?": [
+                "Sacramento",
+				"Los Angeles",
+                "San Francisco",
+                "Oakland"
+            ]
+        },
+		    {
+            "Where is Yosemite National Park?": [
+                "It is in California, east of San Francisco",
+				"It is in California, east of Los Angeles",
+                "It is in Wyoming, Idaho, and Montana",
+                "It is in California, north of Redding"
+                				
+            ]
+        },
+			 {
+            "Which is not in California?": [
+			    "Grand Canyon national park",
+                "Joshua Tree national park",
+				"Channel Islands national park",
+                "Mojave desert"
+
+            ]
+        },
+        {
+            "Which highway is best known for its beautiful beach scenary along the route?": [
+                "CA-1",
+                "Interstate 5",
+                "Interstate 15",
+				"CA-2"
+            ]
+        }
+    ],
+    "QUESTIONS_DE_DE" : [
+        {
+            "Rentiere haben ein sehr dickes Fell. Wie viele Haare pro Quadratzentimeter haben sie?": [
+                "13,000",
+                "1,200",
+                "5,000",
+                "700",
+                "1,000",
+                "120,000"
+            ]
+        }
+    ]
+};
